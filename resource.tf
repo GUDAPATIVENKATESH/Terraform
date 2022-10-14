12945942d44a4d9bb47dbe72cd6548d38ca54687
Venkatesh <gudapativenkatesh4@gmail.com>
2022-10-14 17:20:55 +0530

    added jenkinsfile

--- resource.tf
+++ resource.tf
@@ -6,3 +6,11 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags = {
+    Name = "from_tf"
+  }
+}
