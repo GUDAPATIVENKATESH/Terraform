7a044a0a172eacfb2d01e4221269ee5cf224a35b
Venkatesh <gudapativenkatesh4@gmail.com>
2022-10-21 08:03:15 +0530

    added changes

--- subnets.tf
+++ subnets.tf
@@ -1,72 +1,11 @@
-#Subnet_01
-resource "aws_subnet" "sub1" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.cidr_block_sub1
+#Subnet
+resource "aws_subnet" "subnets" {
+  count             = length(var.names)
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block_vpc, 8, count.index)
+  availability_zone = var.availability_zone[count.index]
 
   tags = {
-    Name = "Subnet1FromTF"
+    Name            = var.names[count.index]
   }
-  depends_on = [
-    aws_vpc.my_vpc
-  ]
-}
-#Subnet_02
-resource "aws_subnet" "sub2" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.cidr_block_sub2
-
-  tags = {
-    Name = "Subnet2FromTF"
-  }
-  depends_on = [
-    aws_subnet.sub1
-  ]
-}
-#Subnet_03
-resource "aws_subnet" "sub3" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.cidr_block_sub3
-
-  tags = {
-    Name = "Subnet3FromTF"
-  }
-  depends_on = [
-    aws_subnet.sub2
-  ]
-}
-#Subnet_04
-resource "aws_subnet" "sub4" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.cidr_block_sub4
-
-  tags = {
-    Name = "Subnet4FromTF"
-  }
-  depends_on = [
-    aws_subnet.sub3
-  ]
-}
-#Subnet_05
-resource "aws_subnet" "sub5" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.cidr_block_sub5
-
-  tags = {
-    Name = "Subnet5FromTF"
-  }
-  depends_on = [
-    aws_subnet.sub4
-  ]
-}
-#Subnet_06
-resource "aws_subnet" "sub6" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.cidr_block_sub6
-
-  tags = {
-    Name = "Subnet6FromTF"
-  }
-  depends_on = [
-    aws_subnet.sub5
-  ]
 }
--- variables.tf
+++ variables.tf
@@ -1,41 +1,20 @@
-#for provider
-variable "provider" {
+#region
+variable "region" {
   type = string
-  default = "us-west-2"
-
 }
 #for VPC
 variable "cidr_block_vpc" {
   type = string
-  default = "10.10.0.0/16"
 }
 #for SUBNET01
-variable "cidr_block_sub1" {
-  type = string
-  default = "10.10.0.0/24"
+#variable "cidr" {
+#  type = list(string)
+#}
+#for availabiltyzone
+variable "availability_zone" {
+  type = list(string)
 }
-#for SUBNET02
-variable "cidr_block_sub2" {
-  type = string
-  default = "10.10.1.0/24"
-}
-#for SUBNET03
-variable "cidr_block_sub3" {
-  type = string
-  default = "10.10.2.0/24"
-}
-#for SUBNET04
-variable "cidr_block_sub4" {
-  type = string
-  default = "10.10.3.0/24"
-}
-#for SUBNET05
-variable "cidr_block_sub5" {
-  type = string
-  default = "10.10.4.0/24"
-}
-#for SUBNET06
-variable "cidr_block_sub6" {
-  type = string
-  default = "10.10.5.0/24"
+#name tags
+variable "names" {
+  type = list(string)
 }
